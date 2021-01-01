ec7da2d2720a2dfce9f6a3d605b34a8b35f47679
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-01-01 00:54:55 -0500

    cross toolchain binutils

--- default.nix
+++ default.nix
@@ -8,15 +8,39 @@ stdenv.mkDerivation {
   configurePhase = ''
   '';
   buildPhase = ''
-    # Note: LFS specifies that /usr/bin/awk should be a symlink to gawk and /usr/bin/yacc should be a symlink to bison, but we are not doing that
+  '';
+  installPhase = ''
+    # LFS specifies that /usr/bin/awk should be a symlink to gawk and /usr/bin/yacc should be a symlink to bison, but we are not doing that
     echo $(which bash)
     echo $(which sh)
     echo $(which awk)
     echo $(which yacc)
-    
-  '';
-  installPhase = ''
-    mkdir -p $out
-    echo install phase >$out/hello.txt
+    set +h
+    umask 022
+    LFS=$out
+    LC_ALL=POSIX
+    echo $(uname -m)
+    LFS_TGT=$(uname -m)-lfs-linux-gnu
+    # LFS specifies that PATH should be /bin:/usr/bin but we are not doing that
+    PATH=$LFS/tools/bin:$PATH
+    echo PATH=$PATH
+    export LFS LC_ALL LFS_TGT PATH
+    mkdir -p $LFS/tools/bin
+
+    #
+    # Build cross toolchain
+    #
+
+    cd binutils-2.35
+    mkdir build
+    cd build
+    ../configure --prefix $LFS/tools \
+                 --with-sysroot=$LFS \
+                 --target=$LFS_TGT \
+                 --disable-nls \
+                 --disable-werror
+    make
+    make install
+    cd ../..
   '';
 }
