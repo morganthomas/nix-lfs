48f63d25a73c565731a198f1b273dc1acade71a3
Morgan Thomas <morgan.a.s.thomas@gmail.com>
2021-01-01 01:27:24 -0500

    WIP gcc

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
+# This assumes x86_64
 with (import <nixpkgs> {});
 stdenv.mkDerivation {
   name = "lfs";
@@ -42,5 +43,40 @@ stdenv.mkDerivation {
     make
     make install
     cd ../..
+
+
+    cd gcc-10.2.0
+    mv ../mpfr-4.1.0 mpfr
+    mv ../gmp-6.2.0 gmp
+    mv ../mpc-1.1.0 mpc
+    sed -e '/m64=/s/lib64/lib/' \
+        -i.orig gcc/config/i386/t-linux64
+    mkdir build
+    cd build
+    ../configure \
+        --target=$LFS_TGT                              \
+        --prefix=$LFS/tools                            \
+        --with-glibc-version=2.11                      \
+        --with-sysroot=$LFS                            \
+        --with-newlib                                  \
+        --without-headers                              \
+        --enable-initfini-array                        \
+        --disable-nls                                  \
+        --disable-shared                               \
+        --disable-multilib                             \
+        --disable-decimal-float                        \
+        --disable-threads                              \
+        --disable-libatomic                            \
+        --disable-libgomp                              \
+        --disable-libquadmath                          \
+        --disable-libssp                               \
+        --disable-libvtv                               \
+        --disable-libstdcxx                            \
+        --enable-languages=c,c++
+    make
+    make install
+    cd ..
+    cat gcc/limitx.h gcc/glimits.h gcc/limity.h > \
+        `dirname $($LFS_TGT-gcc -print-libgcc-file-name)`/install-tools/include/limits.h
   '';
 }
